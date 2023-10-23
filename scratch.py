6f20410ba771c3e99ea18a7d6172816e0bc56352
olivergraininge <oliver@grainge.me>
2023-10-23 09:26:59 +0100

    changes to scratch

--- scratch.py
+++ scratch.py
@@ -1,7 +1,19 @@
-from PlaceRec.Datasets import GardensPointWalking, SFU, StLucia_small
+from PlaceRec.Datasets import GardensPointWalking
+from PlaceRec.Methods import NetVLAD 
 
-method = GardensPointWalking()
+method = NetVLAD()
+dataset = GardensPointWalking()
 
-method = SFU()
+# get query and map loaders for batch inference on images
+query_loader = dataset.query_images_loader("train", preprocess=method.preprocess)
+map_loader = dataset.map_images_loader("train", preprocess=method.preprocess)
 
-method = StLucia_small()
+# compute the image descriptors
+query_desc = method.compute_query_desc(dataloader=query_loader)
+map_desc = method.compute_map_desc(dataloader=map_loader)
+
+# to compute the similarity matrix
+similarity_matrix = method.similarity_matrix(query_desc, map_desc)
+
+# to perform place recognition 
+idx, scores = method.place_recognition(dataloader=query_loader)
