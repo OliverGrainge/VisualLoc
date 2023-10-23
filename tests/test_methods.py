b7cfa44e7d5fd310fbd0732c2885bcff7740ce4c
olivergraininge <oliver@grainge.me>
2023-10-23 13:21:24 +0100

    styling changes

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,20 +1,20 @@
-import unittest 
-from PlaceRec.Methods import NetVLAD, CALC
-from PlaceRec.Datasets import GardensPointWalking
-import torch
-import argparse 
-from PlaceRec.utils import get_method
-# List all method modules you want to test
+import argparse
+import unittest
 
+import torch
+
+from PlaceRec.Datasets import GardensPointWalking
+from PlaceRec.Methods import CALC, NetVLAD
+from PlaceRec.utils import get_method
+
+# List all method modules you want to test
 
 
 methods_to_test = ["amosnet", "hybridnet", "cosplace", "hog", "netvlad", "calc", "convap"]
 
 
-
 def create_test_case(method_module):
     class CustomTestMethod(unittest.TestCase):
-        
         def setUp(self):
             self.method = method_module
             self.dataset = GardensPointWalking()
@@ -24,7 +24,7 @@ def create_test_case(method_module):
 
         def test_name_str(self):
             assert isinstance(self.method.name, str), "Method name must be a string"
-        
+
         def test_name_lower(self):
             assert self.method.name.islower(), "Method name must be lower case"
 
@@ -38,8 +38,8 @@ def create_test_case(method_module):
             query_loader = self.dataset.query_images_loader("test", preprocess=self.method.preprocess)
             query_desc = self.method.compute_query_desc(dataloader=query_loader, pbar=False)
             self.assertIn("query_descriptors", query_desc)
-            assert self.method.query_desc is not None 
-        
+            assert self.method.query_desc is not None
+
         def test_map(self):
             map_loader = self.dataset.query_images_loader("test", preprocess=self.method.preprocess)
             map_desc = self.method.compute_map_desc(dataloader=map_loader, pbar=False)
@@ -64,18 +64,18 @@ def create_test_case(method_module):
             assert (q_desc["query_descriptors"] == new_q_desc["query_descriptors"]).all()
             assert (m_desc["map_descriptors"] == new_m_desc["map_descriptors"]).all()
 
-
     # Rename the test class to include the method module's name for clarity
     CustomTestMethod.__name__ = f"Test{method_module.name}"
-    
+
     return CustomTestMethod
+
 
 # Generate test classes dynamically and set them in the global namespace
 for method_name in methods_to_test:
     method = get_method(method_name)
     test_class = create_test_case(method)
     globals()[test_class.__name__] = test_class
-   
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
