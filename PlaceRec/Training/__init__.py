dcb4c98b36440a4130795fd9be776941abb890ec
olivergraininge <oliver@grainge.me>
2023-10-26 21:47:19 +0100

    adding training code

---
+++ PlaceRec/Training/__init__.py
@@ -0,0 +1,2 @@
+from .constrastive_trainer import ConstrastiveTrainer
+from .contrastive_dataset import 
--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 import argparse
 import yaml
+from PlaceRec.utils import get_method
+from PlaceRec.Training import ConstrastiveTrainer
 
 with open('config.yaml', 'r') as file:
     args = yaml.safe_load(file)
@@ -10,6 +12,12 @@ parser = argparse.ArgumentParser(
     description="Training Visual Geolocalization",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
+parser.add_argument(
+    "--method",
+    type=str,
+    default=train_args["train"]["method"],
+    help="The VPR method to train",
+)
 # Training parameters
 parser.add_argument(
     "--train_batch_size",
@@ -18,15 +26,13 @@ parser.add_argument(
     help="Number of triplets (query, pos, negs) in a batch. Each triplet consists of 12 images",
 )
 
-
-
-
 parser.add_argument(
     "--infer_batch_size",
     type=int,
     default=train_args["infer_batch_size"],
     help="Batch size for inference (caching and testing)",
 )
+
 parser.add_argument(
     "--criterion",
     type=str,
@@ -34,42 +40,67 @@ parser.add_argument(
     help="loss to be used",
     choices=["triplet", "sare_ind", "sare_joint"],
 )
-parser.add_argument(
-    "--margin", type=float, default=train_args["margin"], help="margin for the triplet loss"
-)
-parser.add_argument(
-    "--epochs_num", type=int, default=train_args["epochs_num"], help="number of epochs to train for"
-)
-parser.add_argument("--patience", type=int, default=train_args["patience"])
-parser.add_argument("--lr", type=float, default=train_args["lr"], help="_")
 
 parser.add_argument(
-    "--optim", type=str, default=train_args["optim"], help="_", choices=["adam", "sgd"]
+    "--margin",
+    type=float,
+    default=train_args["margin"], 
+    help="margin for the triplet loss"
 )
+
+parser.add_argument(
+    "--epochs_num", 
+    type=int, 
+    default=train_args["epochs_num"], 
+    help="number of epochs to train for"
+)
+
+parser.add_argument("--patience", 
+    type=int, 
+    default=train_args["patience"]
+)
+
+parser.add_argument("--lr", 
+    type=float, 
+    default=train_args["lr"], 
+    help="_"
+)
+
+parser.add_argument(
+    "--optim", 
+    type=str, 
+    default=train_args["optim"], 
+    help="_", choices=["adam", "sgd"]
+)
+
 parser.add_argument(
     "--cache_refresh_rate",
     type=int,
     default=train_args["cache_refresh_rate"],
     help="How often to refresh cache, in number of queries",
 )
+
 parser.add_argument(
     "--queries_per_epoch",
     type=int,
     default=train_args["queries_per_epoch"],
     help="How many queries to consider for one epoch. Must be multiple of cache_refresh_rate",
 )
+
 parser.add_argument(
     "--negs_num_per_query",
     type=int,
     default=train_args["negs_num_per_query"],
     help="How many negatives to consider per each query in the loss",
 )
+
 parser.add_argument(
     "--neg_samples_num",
     type=int,
     default=train_args["neg_samples_num"],
     help="How many negatives to use to compute the hardest ones",
 )
+
 parser.add_argument(
     "--mining",
     type=str,
@@ -77,10 +108,12 @@ parser.add_argument(
     choices=["partial", "full", "random", "msls_weighted"],
 )
 
-
-
 # Initialization parameters
-parser.add_argument("--seed", type=int, default=train_args["seed"])
+parser.add_argument("--seed", 
+    type=int, 
+    default=train_args["seed"]
+)
+
 parser.add_argument(
     "--resume",
     type=str,
@@ -88,18 +121,27 @@ parser.add_argument(
     help="Path to load checkpoint from, for resuming training or testing.",
 )
 # Other parameters
-parser.add_argument("--device", type=str, default=train_args["device"], choices=["cuda", "cpu"])
-parser.add_argument(
-    "--num_workers", type=int, default=train_args["num_workers"], help="num_workers for all dataloaders"
+parser.add_argument("--device", 
+    type=str, 
+    default=train_args["device"], 
+    choices=["cuda", "cpu"]
 )
+
+parser.add_argument(
+    "--num_workers", 
+    type=int, 
+    default=train_args["num_workers"], 
+    help="num_workers for all dataloaders"
+)
+
 parser.add_argument(
     "--resize",
     type=int,
     default=train_args["resize"],
-    #default=[240, 320],
     nargs=2,
     help="Resizing shape for images (HxW).",
 )
+
 parser.add_argument(
     "--test_method",
     type=str,
@@ -115,10 +157,19 @@ parser.add_argument(
     help="This includes pre/post-processing methods and prediction refinement",
 )
 
-parser.add_argument("--val_positive_dist_threshold", type=int, default=train_args["val_positive_dist_threshold"], help="_")
-parser.add_argument(
-    "--train_positives_dist_threshold", type=int, default=train_args["train_positives_dist_threshold"], help="_"
+parser.add_argument("--val_positive_dist_threshold", 
+    type=int, 
+    default=train_args["val_positive_dist_threshold"], 
+    help="_"
 )
+
+parser.add_argument(
+    "--train_positives_dist_threshold", 
+    type=int, 
+    default=train_args["train_positives_dist_threshold"], 
+    help="_"
+)
+
 parser.add_argument(
     "--recall_values",
     type=int,
@@ -142,6 +193,7 @@ parser.add_argument(
     default=train_args["datasets_folder"],
     help="Path with all datasets",
 )
+
 parser.add_argument(
     "--dataset_name",
     type=str,
@@ -155,7 +207,11 @@ parser.add_argument(
     default=train_args["save_dir"],
     help="Folder name of the current run (saved in ./logs/)",
 )
+
 args = parser.parse_args()
 
+##################################### Training ###################
 
-
+method = get_method(args.method)
+trainer = ConstrastiveTrainer(args, method)
+trainer.fit()
