ea1487a40664477fb534308ea50921f553acbcd8
oliver grainge <oliver@grainge.me>
2024-01-31 15:46:22 +0000

    adding training scripts

--- train_models.sh
+++ train_models.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+python train.py --backbone resnet18 --aggregation mixvpr
 python train.py --backbone resnet18 --aggregation netvlad
 python train.py --backbone resnet18 --aggregation convap
-python train.py --backbone resnet18 --aggregation cosplace
-python train.py --backbone resnet18 --aggregation mixvpr
 python train.py --backbone resnet18 --aggregation gem
-python train.py --backbone resnet18 --aggregation spoc
 python train.py --backbone resnet18 --aggregation mac
+
+
+python train.py --backbone resnet50 --aggregation mixvpr
+python train.py --backbone resnet50 --aggregation netvlad
+python train.py --backbone resnet50 --aggregation convap
+python train.py --backbone resnet50 --aggregation gem
+python train.py --backbone resnet50 --aggregation mac
+
+
+python train.py --backbone resnet101 --aggregation mixvpr
+python train.py --backbone resnet101 --aggregation netvlad
+python train.py --backbone resnet101 --aggregation convap
+python train.py --backbone resnet101 --aggregation gem
+python train.py --backbone resnet101 --aggregation mac
