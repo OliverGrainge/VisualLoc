fixed unstructured pruners memory overflow

PlaceRec/Training/GSV_Cities/sparse_unstructured_trainer.py
@@ -232,7 +232,7 @@ def sparsity(method):
     return overall_sparsity
 
 
-def setup_pruner(method, datamodule, args, prune_step=0.05, n_batch_acc=3):
+def setup_pruner(method, datamodule, args, prune_step=0.05, n_batch_acc=100):
     if args.pruning_type == None:
         raise Exception("Must choose a pruning type for unstructured sparse trainer")
     elif args.pruning_type == "magnitude":
@@ -256,7 +256,7 @@ def sparse_unstructured_trainer(args):
     torch.set_float32_matmul_precision("medium")
 
     datamodule = GSVCitiesDataModule(
-        batch_size=int(args.batch_size / 4),
+        batch_size=int(args.batch_size / 16),
         img_per_place=4,
         min_img_per_place=4,
         shuffle_all=False,
@@ -330,6 +330,7 @@ def sparse_unstructured_trainer(args):
         )
 
         trainer = pl.Trainer(
+            enable_progress_bar=False,
             devices="auto",
             accelerator="auto",
             strategy="auto",
@@ -340,6 +341,7 @@ def sparse_unstructured_trainer(args):
             check_val_every_n_epoch=1,
             callbacks=[checkpoint_cb, earlystopping_cb],
             reload_dataloaders_every_n_epochs=1,
+            # limit_train_batches=10,
         )
 
         trainer.fit(model=model, datamodule=datamodule)

PlaceRec/Training/GSV_Cities/sparse_utils.py
@@ -89,7 +89,11 @@ class TaylorUnstructuredPruner:
 
         gradients_accumulated = {}
         count = 0
-        for batch in tqdm(val_loader, desc="Computing Gradients for Taylor Importance"):
+        for batch in tqdm(
+            val_loader,
+            total=self.n_batch_acc,
+            desc="Computing Gradients for Taylor Importance",
+        ):
             count += 1
             places, labels = batch
             BS, N, ch, h, w = places.shape
@@ -125,12 +129,33 @@ class TaylorUnstructuredPruner:
         importance_scores = {}
         for name, module in self.model.named_modules():
             if isinstance(module, (nn.Conv2d, nn.Linear)):
-                if module.weight_orig.grad is not None:
-                    importance = (
-                        module.weight_orig.data.abs() * gradients_accumulated[name]
-                    )
+                if name in gradients_accumulated.keys():
+                    importance = module.weight_orig.data.abs() * gradients_accumulated[
+                        name
+                    ].to(module.weight_orig.data.device)
                     importance_scores[name] = importance.view(-1).abs()
         return importance_scores
+
+    def prune_by_taylor_scores(self, importance_scores):
+        for name, module in self.model.named_modules():
+            if name in importance_scores:
+                num_weights_to_prune = int(
+                    self.current_amount * module.weight_orig.data.numel()
+                )
+                _, weights_to_prune = torch.topk(
+                    importance_scores[name], num_weights_to_prune, largest=False
+                )
+
+                mask = torch.ones(
+                    module.weight_orig.data.numel(),
+                    dtype=torch.bool,
+                    device=module.weight_orig.device,
+                )
+                mask[weights_to_prune] = False
+                prune.custom_from_mask(
+                    module, name="weight", mask=mask.reshape(module.weight.shape)
+                )
+                module.weight.data[~mask.reshape(module.weight.shape)] = 0.0
 
     def step(self, val_loader=None):
         # Increase pruning threshold, compute gradients, compute importance, prune
@@ -206,6 +231,7 @@ class HessianUnstructuredPruner:
             loss.backward(retain_graph=True)
 
             # Accumulate squared gradients
+
             for name, module in self.model.named_modules():
                 if (
                     isinstance(module, (nn.Conv2d, nn.Linear))
@@ -230,12 +256,12 @@ class HessianUnstructuredPruner:
         importance_scores = {}
         for name, module in self.model.named_modules():
             if isinstance(module, (nn.Conv2d, nn.Linear)):
-                if module.weight_orig.grad is not None:
+                if name in gradients_squared.keys():
                     # Second-order importance approximation (Taylor series expansion)
                     importance = (
                         module.weight_orig.data.abs()
                         * module.weight_orig.grad.data.abs()
-                        * gradients_squared[name]
+                        * gradients_squared[name].to(module.weight_orig.data.device)
                     )
                     importance_scores[name] = importance.view(-1).abs()
         return importance_scores