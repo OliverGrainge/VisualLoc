e49c83dd47b4c1e8c8a4afd619d429336eb8c2df
oliver grainge <oliver@grainge.me>
2024-05-30 09:17:54 +0100

    adding run script

--- myscript.sh
+++ myscript.sh
@@ -5,3 +5,24 @@ python train.py --method ResNet50_GeM --enable_progress_bar --aggregation_prunin
 python train.py --method ResNet50_GeM --enable_progress_bar --aggregation_pruning_rate 2.0
 python train.py --method ResNet50_GeM --enable_progress_bar --aggregation_pruning_rate 0.5
 python train.py --method ResNet50_GeM --enable_progress_bar --aggregation_pruning_rate 0.0
+
+
+python train.py --method MixVPR --enable_progress_bar --aggregation_pruning_rate 1.0
+python train.py --method MixVPR --enable_progress_bar --aggregation_pruning_rate 1.5
+python train.py --method MixVPR --enable_progress_bar --aggregation_pruning_rate 2.0
+python train.py --method MixVPR --enable_progress_bar --aggregation_pruning_rate 0.5
+python train.py --method MixVPR --enable_progress_bar --aggregation_pruning_rate 0.0
+
+
+#python train.py --method ConvAP --enable_progress_bar --aggregation_pruning_rate 1.0
+#python train.py --method ConvAP --enable_progress_bar --aggregation_pruning_rate 1.5
+#python train.py --method ConvAP --enable_progress_bar --aggregation_pruning_rate 2.0
+#python train.py --method ConvAP --enable_progress_bar --aggregation_pruning_rate 0.5
+#python train.py --method ConvAP --enable_progress_bar --aggregation_pruning_rate 0.0
+
+
+python train.py --method NetVLAD --enable_progress_bar --aggregation_pruning_rate 1.0
+python train.py --method NetVLAD --enable_progress_bar --aggregation_pruning_rate 1.5
+python train.py --method NetVLAD --enable_progress_bar --aggregation_pruning_rate 2.0
+python train.py --method NetVLAD --enable_progress_bar --aggregation_pruning_rate 0.5
+python train.py --method NetVLAD --enable_progress_bar --aggregation_pruning_rate 0.0
