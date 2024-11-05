55f21b0ae9c50a45434eb969c38bc67563445dfb
olivergraininge <oliver@grainge.me>
2024-11-05 08:57:55 +0000

    fixing code

--- PlaceRec/Methods/__init__.py
+++ PlaceRec/Methods/__init__.py
@@ -1,11 +1,5 @@
-from ...bin.anyloc import AnyLoc
-from ...bin.cosplace import CosPlace
-from ...bin.dino_salad import DinoSalad
-from ...bin.eigenplaces import EigenPlaces
-from .base_method import SingleStageBaseModelWrapper, TwoStageBaseModelWrapper
+from .base_method import SingleStageBaseModelWrapper
 from .convap import ConvAP, ResNet34_ConvAP
-from .gem import ResNet34_GeM, ResNet50_GeM
+from .gem import ResNet50_GeM, ResNet34_GeM
 from .mixvpr import MixVPR, ResNet34_MixVPR
 from .netvlad import NetVLAD, ResNet34_NetVLAD
-from .selavpr import SelaVPR
-from .sfrs import SFRS
